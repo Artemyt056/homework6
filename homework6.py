25f3d2a27c7dbc80a990125111d88b3700e44125
Artemyt <artemyt056@gmail.com>
2023-09-15 13:50:17 +0300

    homework6

--- homework6.py
+++ homework6.py
@@ -1,20 +1,5 @@
-list_of_city = [
-    'Kyiv',
-    'Odesa',
-    'Lviv',
-    'Zhytomyr',
-    'Uzhhorod',
-    'Kharkiv',
-    'Slavutych',
-]
+cities = "київ,оДеса     Львів.житоМИР,уЖгОрОд.....ХарКІВ       , слАвУтИч".replace(',', ' ').replace('.', ' ').title().split()
 
-first_city = list_of_city[0]
-second_city = list_of_city[1]
-third_city = list_of_city[2]
-forth_city = list_of_city[3]
-fifth_city = list_of_city[4]
-sixth_city = list_of_city[5]
-seventh_city = list_of_city[6]
 
-for city in list_of_city:
+for city in cities:
     print(f"Я\U00002764_{city}_")
